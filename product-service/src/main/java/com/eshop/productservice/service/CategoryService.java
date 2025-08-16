42a8852ecb340cde9c0a88ba9cbc228993870ef6
Prodocc <tobi75500@gmail.com>
2025-08-16 22:40:43 +0300

    add get all categories endpoint, add category mapper, change return values from actual Object to Dto everywhere

--- product-service/src/main/java/com/eshop/productservice/service/CategoryService.java
+++ product-service/src/main/java/com/eshop/productservice/service/CategoryService.java
@@ -1,4 +1,26 @@
 package com.eshop.productservice.service;
 
+import com.eshop.productservice.dto.CategoryDto;
+import com.eshop.productservice.mappers.CategoryMapper;
+import com.eshop.productservice.model.Category;
+import com.eshop.productservice.repository.CategoryRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
+
+    public CategoryService(CategoryRepository categoryRepository, CategoryMapper categoryMapper) {
+        this.categoryRepository = categoryRepository;
+        this.categoryMapper = categoryMapper;
+    }
+
+    public List<CategoryDto> findAllCategories() {
+        return categoryRepository.findAll().stream().map(categoryMapper::toDto).toList();
+    }
 }
