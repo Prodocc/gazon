4e3219d8bfd3483083aac3b5390e27297c393394
Prodocc <tobi75500@gmail.com>
2025-08-16 20:44:52 +0300

    add getAllProducts list paginated

--- product-service/src/main/java/com/eshop/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/eshop/productservice/controller/ProductController.java
@@ -1,27 +1,49 @@
 package com.eshop.productservice.controller;
 
-import com.eshop.productservice.dto.ProductCreateRequest;
+import com.eshop.productservice.dto.ProductDto;
 import com.eshop.productservice.model.Product;
 import com.eshop.productservice.service.ProductService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductController {
-    private final ProductService service;
+    private final ProductService productService;
 
-    public ProductController(ProductService service){
-        this.service = service;
+    public ProductController(ProductService service) {
+        this.productService = service;
     }
 
     @PostMapping
-    public ResponseEntity<Product> createProduct(@RequestBody ProductCreateRequest request) {
-        Product created = service.createProduct(request);
+    public ResponseEntity<Product> createProduct(@RequestBody ProductDto request) {
+        Product created = productService.createProduct(request);
         return ResponseEntity.ok(created);
+    }
+
+    @GetMapping
+    public Page<ProductDto> getProductsPaginated(
+            @RequestParam int page,
+            @RequestParam int size) {
+
+        Page<Product> productPage = productService.findProductsPaginated(page, size);
+
+        return productPage.map(this::convertToDto);
+
+    }
+
+    private ProductDto convertToDto(Product product) {
+        ProductDto dto = new ProductDto();
+        dto.setName(product.getName());
+        dto.setDescription(product.getDescription());
+        dto.setPrice(product.getPrice());
+        dto.setQuantity(product.getQuantity());
+
+        return dto;
     }
 
 }
